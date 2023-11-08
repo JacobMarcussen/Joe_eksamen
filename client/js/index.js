fc9c8720a8fcd37b1e0a5ca69f81777af48efd64
gsalhoej <ngustavsalhoej@gmail.com>
2023-11-08 12:49:31 +0100

    close logout tap

--- client/js/index.js
+++ client/js/index.js
@@ -1,3 +1,8 @@
 function myFunction(x) {
   x.classList.toggle("change");
 }
+
+document.getElementById('closeButton').addEventListener('click', function() {
+  document.getElementById('confirmationOverlay').style.display = 'none';
+});
+
